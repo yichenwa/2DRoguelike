b1eb5fd2451a05a48c0b3b57ac56eaa64fcd68ff
mike4tin <31666282+mike4tin@users.noreply.github.com>
2017-09-20 15:47:21 -0400

    Player can move with animation

---
+++ EnemyScript.cs
@@ -0,0 +1,53 @@
+﻿// Mike Fortin, Basic Enemy Script
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody2D))]
+[RequireComponent (typeof (BoxCollider2D))]
+public class EnemyScript : MonoBehaviour {
+
+	public float enemySpeed = .75f;
+	private Vector2 playerPosition;
+	private PlayerScript2 player;
+	private Rigidbody2D enemyBody;
+	private bool canWalk = true;
+	// Use this for initialization
+	void Start () 
+	{
+		player = FindObjectOfType<PlayerScript2> ();
+		enemyBody = FindObjectOfType<Rigidbody2D> ();
+		enemyBody.freezeRotation = true;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		if (canWalk == false) 
+		{
+			StartCoroutine (walkDelay());
+		}
+		if (canWalk == true) 
+		{
+			playerPosition = (player.transform.position);
+			transform.position = Vector2.MoveTowards (transform.position, playerPosition, enemySpeed * Time.deltaTime); 
+		}
+
+	}
+		
+	void OnCollisionEnter2D(Collision2D thing)
+	{
+		if (thing.gameObject.name == "Player") 
+		{
+			player.playerHP -= 50;
+		}
+
+		canWalk = false;
+	}
+
+	IEnumerator walkDelay()
+	{
+		yield return new WaitForSeconds (.5f);
+		canWalk = true;
+	}
+}
---
+++ PlayerScript2.cs
@@ -0,0 +1,50 @@
+﻿//Mike Fortin, Basic Player Script
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody2D))]
+[RequireComponent (typeof (BoxCollider2D))]
+public class PlayerScript2 : MonoBehaviour {
+
+	public float playerSpeed = 3f;
+	public float playerHP = 100;
+	private Rigidbody2D playerBody;
+	private Animator animator;
+	// Use this for initialization
+	void Start () 
+	{
+		playerBody = FindObjectOfType<Rigidbody2D> ();
+		playerBody.freezeRotation = true;
+		animator = FindObjectOfType<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector2 targetVelocity = new Vector2 (Input.GetAxisRaw ("Horizontal"), 
+			                         Input.GetAxisRaw ("Vertical"));
+		GetComponent<Rigidbody2D> ().velocity = targetVelocity * playerSpeed;
+
+		if (targetVelocity.y > 0) 
+		{
+			animator.SetTrigger ("back");
+		}
+		if (targetVelocity.y < 0)
+		{
+			animator.SetTrigger("forward");
+		}
+		if (targetVelocity.x < 0) 
+		{
+			animator.SetTrigger ("left");
+		}
+		if (targetVelocity.x > 0) 
+		{
+			animator.SetTrigger ("right");
+		}
+
+		if (playerHP <= 0) 
+		{
+			this.gameObject.SetActive(false);
+		}
+	}
+}
