8c537963506248a1808c6ee8780ca28b21b236a5
mike4tin <msfortin@buffalo.edu>
2017-09-21 14:21:41 -0400

    Commented scripts
    Animation fix to prevent craziness on diagonal movement

--- EnemyScript.cs
+++ EnemyScript.cs
@@ -1,4 +1,5 @@
 ﻿// Mike Fortin, Basic Enemy Script
+//All code in this file written by Mike Fortin thru 9/21
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,12 +8,13 @@ using UnityEngine;
 [RequireComponent (typeof (BoxCollider2D))]
 public class EnemyScript : MonoBehaviour {
 
-	public float enemySpeed = .75f;
-	private Vector2 playerPosition;
-	private PlayerScript2 player;
-	private Rigidbody2D enemyBody;
-	private bool canWalk = true;
-	// Use this for initialization
+	public float enemySpeed = .75f; //Used to set the enemies walking speed
+	private Vector2 playerPosition; //Will be used to locate the player's location
+	private PlayerScript2 player; //Finds the player object by finding the PlayerScript2 object
+	private Rigidbody2D enemyBody; //Used to control the enemy body 
+	private bool canWalk = true; //Will be used to make the enemy pause if a condition is met 
+
+	// Used for initialization
 	void Start () 
 	{
 		player = FindObjectOfType<PlayerScript2> ();
@@ -20,14 +22,14 @@ public class EnemyScript : MonoBehaviour {
 		enemyBody.freezeRotation = true;
 	}
 	
-	// Update is called once per frame
+	//Main function for the enemy to update
 	void FixedUpdate () 
 	{
 		if (canWalk == false) 
 		{
-			StartCoroutine (walkDelay());
+			StartCoroutine (walkDelay()); //After he's been stopped, call the function to restart his walking
 		}
-		if (canWalk == true) 
+		if (canWalk == true)  //Move towards the player creepily
 		{
 			playerPosition = (player.transform.position);
 			transform.position = Vector2.MoveTowards (transform.position, playerPosition, enemySpeed * Time.deltaTime); 
@@ -35,17 +37,17 @@ public class EnemyScript : MonoBehaviour {
 
 	}
 		
-	void OnCollisionEnter2D(Collision2D thing)
+	void OnCollisionEnter2D(Collision2D thing) //If the enemy collides with the player, inflict damage to him
 	{
 		if (thing.gameObject.name == "Player") 
 		{
-			player.playerHP -= 50;
+			player.playerHP -= 50; //Damage done, can be adjusted
 		}
 
 		canWalk = false;
 	}
 
-	IEnumerator walkDelay()
+	IEnumerator walkDelay() //Used to get the enemy walking after a collision
 	{
 		yield return new WaitForSeconds (.5f);
 		canWalk = true;
--- PlayerScript2.cs
+++ PlayerScript2.cs
@@ -1,4 +1,5 @@
 ﻿//Mike Fortin, Basic Player Script
+//All code in this file written by Mike Fortin thru 9/21
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,11 +8,12 @@ using UnityEngine;
 [RequireComponent (typeof (BoxCollider2D))]
 public class PlayerScript2 : MonoBehaviour {
 
-	public float playerSpeed = 3f;
-	public float playerHP = 100;
-	private Rigidbody2D playerBody;
-	private Animator animator;
-	// Use this for initialization
+	public float playerSpeed = 3f; //Used to set the players speed when moving
+	public float playerHP = 100; //Used to store adjust and update the players health
+	private Rigidbody2D playerBody; //Used to find and adjust the body of the player
+	private Animator animator; //Used to set animation triggers off depending on movement
+
+	// Used for initialization
 	void Start () 
 	{
 		playerBody = FindObjectOfType<Rigidbody2D> ();
@@ -19,29 +21,33 @@ public class PlayerScript2 : MonoBehaviour {
 		animator = FindObjectOfType<Animator> ();
 	}
 	
-	// Update is called once per frame
+	// This is how the player's movement and status will update 
 	void FixedUpdate () {
 		Vector2 targetVelocity = new Vector2 (Input.GetAxisRaw ("Horizontal"), 
 			                         Input.GetAxisRaw ("Vertical"));
-		GetComponent<Rigidbody2D> ().velocity = targetVelocity * playerSpeed;
+		GetComponent<Rigidbody2D> ().velocity = targetVelocity * playerSpeed; //moves the player
 
-		if (targetVelocity.y > 0) 
-		{
-			animator.SetTrigger ("back");
-		}
-		if (targetVelocity.y < 0)
-		{
-			animator.SetTrigger("forward");
-		}
+		//This series of ifs uses animation triggers according to directional movement
+		//Priority animation given to left and right on diagonal movement
 		if (targetVelocity.x < 0) 
 		{
 			animator.SetTrigger ("left");
 		}
-		if (targetVelocity.x > 0) 
+		else if (targetVelocity.x > 0) 
 		{
 			animator.SetTrigger ("right");
 		}
+		else if (targetVelocity.y > 0) 
+		{
+			animator.SetTrigger ("back");
+		}
+		else if (targetVelocity.y < 0)
+		{
+			animator.SetTrigger("forward");
+		}
 
+
+		//Kills the player if his health drops below 0
 		if (playerHP <= 0) 
 		{
 			this.gameObject.SetActive(false);
