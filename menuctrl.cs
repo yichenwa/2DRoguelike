41002c98a693e7c219a346115d25a84fea730b68
clintono1 <clintono@buffalo.edu>
2017-09-24 13:10:08 -0400

    Update menuctrl.cs

--- menuctrl.cs
+++ menuctrl.cs
@@ -1,3 +1,7 @@
+// Scenes Must be added to the Build Settings if being manually imported.
+// The code references the build settings to find the string parameters passed. 
+// So--->File--->Build Settings--->type in "name of scene created"
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
