af12853db0e02e4620b18d0c90e3b134a5cea86f
mike4tin <msfortin@buffalo.edu>
2017-10-25 12:31:18 -0400

    Elevated path fix

--- Assets/Combat/Script/shooting.cs
+++ Assets/Combat/Script/shooting.cs
@@ -9,7 +9,7 @@ public class shooting : MonoBehaviour {
 	// Use this for initialization
 	public GameObject Bullet; //Finds the bullet prefab
 	private Vector3 leftPositionFix; //Fixes issue when shooting to the left
-	private float leftOffset = 2f; //Offset needed to handle this issue
+	private float leftOffset = 7.5f; //Offset needed to handle this issue
 	//public float shotDelay = 5f; 
 	//private bool shotFired = false;
 	private PlayerScript2 player;
@@ -29,7 +29,7 @@ public class shooting : MonoBehaviour {
 		{   
 			//animator.SetTrigger ("attack"); //We will need left, right, up, down attacking animations
 			if (player.getDirectionString () == "l") {
-				leftPositionFix.Set (transform.position.x - leftOffset, transform.position.y, -1); //Used to fix shooting left issues
+				leftPositionFix.Set (transform.position.x - leftOffset, transform.position.y, -1f); //Used to fix shooting left issues
 				Instantiate (Bullet, leftPositionFix,  new Quaternion(0, 0, 0, 0));
 				animator.SetTrigger ("shootleft");
 			} 
