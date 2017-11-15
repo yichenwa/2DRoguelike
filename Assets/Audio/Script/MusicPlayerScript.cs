cde0c6116733a9deafe60db44e2fca7fbecc960b
mike4tin <msfortin@buffalo.edu>
2017-11-15 11:37:22 -0500

    Updating when music starts and stops

--- Assets/Audio/Script/MusicPlayerScript.cs
+++ Assets/Audio/Script/MusicPlayerScript.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class MusicPlayerScript : MonoBehaviour {
 
 	private GameObject [] musicPlayer;
+
 	// Use this for initialization
 	void Start () 
 	{
 		musicPlayer  = GameObject.FindGameObjectsWithTag ("Music");
 		if (musicPlayer.Length > 1)
 			Destroy (this.gameObject);
-		DontDestroyOnLoad (this.gameObject);
+		//DontDestroyOnLoad (this.gameObject);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
 		
 	}
 }
